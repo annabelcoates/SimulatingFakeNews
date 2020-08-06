790331dc5f3d7f4251fcf0fa39415f02d06354bc
annabelcoates <annabelcoates@gmail.com>
2020-08-06 17:41:53 +0100

    Add files via upload

--- tester_wheel_graph.py
+++ tester_wheel_graph.py
@@ -13,12 +13,12 @@ k=args.k
 G = nx.watts_strogatz_graph(n,k,1)
 gf.write_to_csv(G, "C:/Users/Anni/Documents/Uni/Computer "
                    "Science/Proj/small_world_graph.csv")
-nx.draw(G,with_labels=False,pos=nx.spectral_layout(G),
-       node_size=10,linewidths=0.1,width=0.01,node_color=range(10000),
-      cmap=plt.cm.Blues)
+#nx.draw(G,with_labels=False,pos=nx.spectral_layout(G),
+ #      node_size=10,linewidths=0.1,width=0.01,node_color=range(1000),
+  #    cmap=plt.cm.Blues)
 
-plt.savefig("C:/Users/Anni/Documents/Uni/Computer "
-            "Science/Proj/Network Diagrams/MostRecentNxDrawing.png")
+#plt.savefig("C:/Users/Anni/Documents/Uni/Computer "
+#            "Science/Proj/Network Diagrams/MostRecentNxDrawing.png")
 			
 print("Done with n="+str(n))
 plt.show()
